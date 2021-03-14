dc2a83f853be185197d05661d16d0560bd7c9890
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-03-14 19:52:36 +0300

    Move service default implementation to separate macro, add macro that creates interface dependency for service target (so one service could depend on API of another service, for example).

--- Source/Sober/Service.cmake
+++ Source/Sober/Service.cmake
@@ -18,22 +18,21 @@ If service API headers include implementation-specific headers, library variants
 can not be defined as link-only variants and must be compiled separately.")
 
     set_property (TARGET ${SERVICE_NAME} PROPERTY INTERFACE_USES_IMPLEMENTATION_HEADERS FALSE)
-    unset (SOBER_SERVICE_CONFIGURATION_DONE)
-    unset (SOBER_INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION_SELECTED)
+    unset (SOBER_IMPLEMENTATION_REGISTRATION_STARTED)
 endmacro ()
 
 macro (sober_add_service_include_directory INCLUDE_DIRECTORY)
-    if (SOBER_SERVICE_CONFIGURATION_DONE)
-        message (SEND_ERROR "Sober: caught attempt to add service include \
-                             directory after implementation registrations!")
-    endif ()
-
     message (STATUS "    Adding include directory: \"${INCLUDE_DIRECTORY}\".")
     target_include_directories (${SOBER_SERVICE_NAME} INTERFACE "${INCLUDE_DIRECTORY}")
 endmacro ()
 
+macro (sober_add_service_api_dependency TARGET_NAME)
+    message (STATUS "    Adding API dependency: \"${TARGET_NAME}\".")
+    target_link_libraries (${SOBER_SERVICE_NAME} INTERFACE "${TARGET_NAME}")
+endmacro ()
+
 macro (sober_add_implementation_headers_requirement)
-    if (SOBER_SERVICE_CONFIGURATION_DONE)
+    if (SOBER_IMPLEMENTATION_REGISTRATION_STARTED)
         message (SEND_ERROR
                 "Sober: caught attempt to add implementation headers requirement after implementation registrations!")
     endif ()
@@ -45,31 +44,41 @@ endmacro ()
 
 macro (sober_add_service_implementation IMPLEMENTATION_DIRECTORY)
     message (STATUS "    Adding implementation from \"${IMPLEMENTATION_DIRECTORY}\".")
-    set (SOBER_SERVICE_CONFIGURATION_DONE TRUE)
+    set (SOBER_IMPLEMENTATION_REGISTRATION_STARTED TRUE)
     add_subdirectory (${IMPLEMENTATION_DIRECTORY})
     target_link_libraries ("${SOBER_SERVICE_NAME}${SOBER_SERVICE_IMPLEMENTATION_NAME}" PUBLIC ${SOBER_SERVICE_NAME})
 endmacro ()
 
-macro (sober_add_default_service_implementation IMPLEMENTATION_DIRECTORY)
-    sober_add_service_implementation ("${IMPLEMENTATION_DIRECTORY}")
-    if (SOBER_INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION_SELECTED)
+function (set_default_service_implementation IMPLEMENTATION_NAME)
+    get_property (DEFAULT_IMPLEMENTATION_SELECTED TARGET ${SOBER_SERVICE_NAME}
+                 PROPERTY INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION SET)
+
+    if (DEFAULT_IMPLEMENTATION_SELECTED)
         # TODO: Unable to find better format for such long messages in documentation. Check again.
         message (WARNING "\
-Sober: unable to make \"${SOBER_SERVICE_IMPLEMENTATION_NAME}\" default implementation \
-for service \"${SOBER_SERVICE_NAME}\", because other implementation is already \
-\"${${SOBER_SERVICE_NAME}_DEFAULT_IMPLEMENTATION}}\"!")
+Sober: unable to make \"${IMPLEMENTATION_NAME}\" default implementation for service \
+\"${SOBER_SERVICE_NAME}\", because other implementation is already selected as default!")
+        return ()
+    endif ()
 
+    if (NOT TARGET "${SOBER_SERVICE_NAME}${IMPLEMENTATION_NAME}")
+        message (SEND_ERROR "\
+Sober: unable to make \"${IMPLEMENTATION_NAME}\" default implementation for service \
+\"${SOBER_SERVICE_NAME}\", because there is no target with name \"${SOBER_SERVICE_NAME}${IMPLEMENTATION_NAME}\"!")
+        return ()
     endif ()
 
     set_property (TARGET ${SOBER_SERVICE_NAME}
-                  PROPERTY INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION ${SOBER_SERVICE_IMPLEMENTATION_NAME})
+                  PROPERTY INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION ${IMPLEMENTATION_NAME})
 
-    set (SOBER_INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION_SELECTED TRUE)
     message (STATUS "        Selected as default implementation!")
-endmacro ()
+endfunction ()
 
 macro (sober_end_service)
-    if (NOT SOBER_INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION_SELECTED)
+    get_property (DEFAULT_IMPLEMENTATION_SELECTED TARGET ${SOBER_SERVICE_NAME}
+            PROPERTY INTERFACE_SERVICE_DEFAULT_IMPLEMENTATION SET)
+
+    if (NOT DEFAULT_IMPLEMENTATION_SELECTED)
         message (FATAL_ERROR "Sober: default implementation for \"${SOBER_SERVICE_NAME}\" is not specified!")
     endif ()
 
