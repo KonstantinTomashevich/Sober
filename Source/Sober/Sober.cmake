8795150f4af9af259cfba3b5d5cb6aed9509950c
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-03-17 21:12:09 +0300

    Library: add SOBER_VARIANT_FULL_NAME to make code more readable.

--- Source/Sober/Sober.cmake
+++ Source/Sober/Sober.cmake
@@ -1,3 +1,4 @@
 include_guard (GLOBAL)
+message (STATUS "Including Sober framework. Branch: master.")
 include (${CMAKE_CURRENT_LIST_DIR}/Library.cmake)
 include (${CMAKE_CURRENT_LIST_DIR}/Service.cmake)
