e44f101ebdcfdd13183176cf1b5bd266f892966f
Konstantin Tomashevich <ekit.mail@gmail.com>
2021-04-24 13:45:30 +0300

    Add missing comments.

---
+++ Stub.cpp
@@ -0,0 +1,2 @@
+// Stub file, used to setup executable targets, that are fully composed from linked libraries.
+// Example: service implementation test runners.
--- Test.cmake
+++ Test.cmake
@@ -1,12 +1,17 @@
-# [WIP]
-# Test.cmake contains library wrapper, that simlifies service testing configration.
+# Test.cmake contains macros, that help to setup service implementation agnostic tests as sober library and
+# ctest executables. Macros are used instead of functions, because these module provides wrappers, that add test-related
+# functionality, to library and variant setup functions.
 
 include_guard (GLOBAL)
 include (${CMAKE_CURRENT_LIST_DIR}/Naming.cmake)
 include (${CMAKE_CURRENT_LIST_DIR}/Library.cmake)
 
+# Every CMake executable must have at least one source translation unit. Sober service test runners
+# do not have any, because they are used to represent variant library as executable.
 set (SOBER_TEST_RUNNER_STUB_SOURCE "${CMAKE_CURRENT_LIST_DIR}/Stub.cpp")
 
+# Wraps sober_library_begin, SERVICE_NAME must be equal to the name of service, that will be tested using these tests.
+# Given service will be automatically used within private scope.
 macro (sober_test_begin SERVICE_NAME)
     message (STATUS "Creating tests library for service \"${SERVICE_NAME}\".")
     set (SOBER_TEST_SERVICE_NAME "${SERVICE_NAME}")
@@ -17,6 +22,7 @@ macro (sober_test_begin SERVICE_NAME)
     sober_library_use_service ("${SERVICE_NAME}" PRIVATE)
 endmacro ()
 
+# Wraps sober_library_end, generates and registers test runners for each registered test variant.
 macro (sober_test_end)
     sober_library_end ()
     message (STATUS "Creating tests runners for \"${SOBER_TEST_SERVICE_NAME}\".")
@@ -37,6 +43,8 @@ macro (sober_test_end)
     unset (SOBER_TEST_SERVICE_NAME)
 endmacro ()
 
+# Wraps sober_variant_begin, registers this variant as test variant, that tests implementation with name
+# equal to IMPLEMENTATION_NAME. Given implementation will be automatically frozen for this variant.
 macro (sober_test_variant_begin IMPLEMENTATION_NAME)
     sober_naming_test_variant ("${IMPLEMENTATION_NAME}" VARIANT_NAME)
     list (APPEND SOBER_TEST_VARIANTS "${VARIANT_NAME}")
@@ -46,6 +54,7 @@ macro (sober_test_variant_begin IMPLEMENTATION_NAME)
     sober_variant_freeze_implementation ("${SOBER_TEST_SERVICE_NAME}" "${IMPLEMENTATION_NAME}")
 endmacro ()
 
+# Wraps sober_variant_end, added for consistency.
 macro (sober_test_variant_end)
     sober_variant_end ()
 endmacro ()
